51f856fcbf0a116d8a71295ae22a0ee4dec673bd
gopala01 <58184372+gopala01@users.noreply.github.com>
2023-03-30 14:17:39 +0100

    Update index.js

--- index.js
+++ index.js
@@ -1,77 +1,102 @@
+// import firebase from "https://www.gstatic.com/firebasejs/9.18.0/firebase.js";
+// import { initializeApp } from "https://www.gstatic.com/firebasejs/9.18.0/firebase-app.js";
+// import { getAuth, createUserWithEmailAndPassword } from "https://www.gstatic.com/firebasejs/9.18.0/firebase-auth.js";
+
+
+// // TODO: Add SDKs for Firebase products that you want to use
+// // https://firebase.google.com/docs/web/setup#available-libraries
+
+// // Your web app's Firebase configuration
+// const firebaseConfig = {
+//     apiKey: "AIzaSyBHIHD0PDT5IPyomztjZXjGY3_AdTsIfj0",
+//     authDomain: "qm-se-20.firebaseapp.com",
+//     projectId: "qm-se-20",
+//     storageBucket: "qm-se-20.appspot.com",
+//     messagingSenderId: "830582484400",
+//     appId: "1:830582484400:web:c2a7f"
+// };
+
+// // Initialize Firebase
+// const app = firebase.initializeApp(firebaseConfig);
+// const auth = getAuth()
+
+
+
+
+// document.getElementById('signupinp').addEventListener('click', function (event) {
+//     function signup(event){
+//         event.preventDefault();
+//     }
+
+//     var firstName = document.getElementById('firstName').value
+//     var lastName = document.getElementById('lastName').value
+//     var email = document.getElementById('email').value
+//     var password = document.getElementById('password').value
+
+//     auth.createUserWithEmailAndPassword(email, password)
+//         .then(function (user) {
+//             var ref = firebase.database().ref("users").child(user.uid)
+//             ref.set({
+//                 firstName: firstName,
+//                 lastName: lastName,
+//                 email: email,
+//                 uid: user.uid
+//             });
+//         })
+//         .catch(function (error) {
+//             var errorCode = error.code;
+//             var errorMessage = error.message;
+//             alert(errorMessage)
+//         });
+// });
+
+import firebase from "https://www.gstatic.com/firebasejs/9.18.0/firebase.js";
 import { initializeApp } from "https://www.gstatic.com/firebasejs/9.18.0/firebase-app.js";
 import { getAuth, createUserWithEmailAndPassword } from "https://www.gstatic.com/firebasejs/9.18.0/firebase-auth.js";
-// import { getAuth } from "https://www.gstatic.com/firebasejs/9.18.0/firebase-database.js";
-
-
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyBHIHD0PDT5IPyomztjZXjGY3_AdTsIfj0",
-  authDomain: "qm-se-20.firebaseapp.com",
-  projectId: "qm-se-20",
-  storageBucket: "qm-se-20.appspot.com",
-  messagingSenderId: "830582484400",
-  appId: "1:830582484400:web:c2a7f77ab61f1344f03a75"
+    apiKey: "AIzaSyBHIHD0PDT5IPyomztjZXjGY3_AdTsIfj0",
+    authDomain: "qm-se-20.firebaseapp.com",
+    projectId: "qm-se-20",
+    storageBucket: "qm-se-20.appspot.com",
+    messagingSenderId: "830582484400",
+    appId: "1:830582484400:web:c2a7f"
 };
 
 // Initialize Firebase
-const app = initializeApp(firebaseConfig);
-const auth = getAuth()
+const app = firebase.initializeApp(firebaseConfig);
+const auth = getAuth();
 
-var firstName = document.getElementById('firstName')
-var lastName = document.getElementById('lastName')
-var email = document.getElementById('email')
-var password = document.getElementById('password')
+function signup(event) {
+  event.preventDefault();
 
+  const firstName = document.getElementById("firstName").value;
+  const lastName = document.getElementById("lastName").value;
+  const email = document.getElementById("email").value;
+  const password = document.getElementById("password").value;
 
-window.signup = function(e){
-    e.preventDefault();
-    var obj = {
-        firstName : firstName.value,
-        lastName : lastName.value,
-        email : email.value,
-        password : password.value,
-        last_login : Date.now()
-    }
-    createUserWithEmailAndPassword(auth, obj.email, obj.password)
-    .then(function(success){
-        alert("Signup success")
+  createUserWithEmailAndPassword(auth, email, password)
+    .then((userCredential) => {
+      const user = userCredential.user;
+
+      // Set user data in the database
+      const ref = firebase.database().ref("users").child(user.uid);
+      ref.set({
+        firstName: firstName,
+        lastName: lastName,
+        email: email,
+        uid: user.uid,
+      });
+
+      // Redirect to a success page
+      window.location.href = "success.html";
     })
-    .catch(function(error){
-        alert("Error " + error)
-    })
-    console.log(obj)
+    .catch((error) => {
+      const errorCode = error.code;
+      const errorMessage = error.message;
+      alert(errorMessage);
+    });
 }
 
-
-// function register(){
-//     firstName = document.getElementById('firstName').value
-//     lastName = document.getElementById('lastName').value
-//     email = document.getElementById('email').value
-
-//     auth.createUserWithEmailAndPassword(email, password)
-//     .then(function(){
-//         var user = auth.currentUser
-
-//         var database_ref = database.ref()
-
-//         var user_data = {
-//             firstName : firstName,
-//             lastName : lastName,
-//             email : email,
-//             last_login : Date.now()
-//         }
-
-//         database_ref.child('users/' + user.uid).set(user_data)
-
-//         alert('User created!')
-//     })
-//     .catch(function(error){
-//         var error_message = error.message
-//         alert(error_message)
-//     })
-
-
-// }
+document.getElementById("signupinp").addEventListener("click", signup);

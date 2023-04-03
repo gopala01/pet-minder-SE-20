2b0c7ac7b5e5e73e9402886b21c67526b090fe72
Arturo Jose Mamba <arturojmamba14@gmail.com>
2023-04-03 16:57:27 +0100

    edits

--- pages/authenticateUsers/signUpUsers.js
+++ pages/authenticateUsers/signUpUsers.js
@@ -1,51 +1,48 @@
-import { initializeApp } from "../../node_modules/firebase/app"
-import { getAuth, createUserWithEmailAndPassword } from "../../node_modules/firebase/auth"
-import { getDatabase, ref, set } from "../../node_modules/firebase/database";
+import { initializeApp } from 'https://www.gstatic.com/firebasejs/9.18.0/firebase-app.js';
+    import { getAuth, createUserWithEmailAndPassword } from 'https://www.gstatic.com/firebasejs/9.18.0/firebase-auth.js';
+    import { getDatabase, ref, set } from 'https://www.gstatic.com/firebasejs/9.18.0/firebase-database.js';
 
-// Your web app's Firebase configuration
-const firebaseConfig = {
-  apiKey: "AIzaSyBHIHD0PDT5IPyomztjZXjGY3_AdTsIfj0",
-  authDomain: "qm-se-20.firebaseapp.com",
-  databaseURL: "https://qm-se-20-default-rtdb.firebaseio.com",
-  projectId: "qm-se-20",
-  storageBucket: "qm-se-20.appspot.com",
-  messagingSenderId: "830582484400",
-  appId: "1:830582484400:web:c2a7f77ab61f1344f03a75"
-};
+    const firebaseConfig = {
+    apiKey: "AIzaSyBHIHD0PDT5IPyomztjZXjGY3_AdTsIfj0",
+    authDomain: "qm-se-20.firebaseapp.com",
+    databaseURL: "https://qm-se-20-default-rtdb.firebaseio.com",
+    projectId: "qm-se-20",
+    storageBucket: "qm-se-20.appspot.com",
+    messagingSenderId: "830582484400",
+    appId: "1:830582484400:web:c2a7f77ab61f1344f03a75"
+    };
 
-// Initialize Firebase
-const app = initializeApp(firebaseConfig);
-const auth = getAuth(app);
-const db = getDatabase(app);
+    // Initialize Firebase
+    const app = initializeApp(firebaseConfig);
+    const auth = getAuth(app);
+    const db = getDatabase(app);
 
-function signup(event) {
-  event.preventDefault();
-  var firstName = document.getElementById("firstName").value;
-  var lastName = document.getElementById("lastName").value;
-  var email = document.getElementById("email").value;
-  var password = document.getElementById("password").value;
-  var passwordMatch = document.getElementById("repassword").value;
+    function signup(event) {
+    event.preventDefault();
+    var firstName = document.getElementById("firstName").value;
+    var lastName = document.getElementById("lastName").value;
+    var email = document.getElementById("email").value;
+    var password = document.getElementById("password").value;
 
-  createUserWithEmailAndPassword(auth, email, password)
-    .then((userCredential) => {
-      const user = userCredential.user;
+    createUserWithEmailAndPassword(auth, email, password)
+        .then((userCredential) => {
+        const user = userCredential.user;
 
-      // Set user data in the database
-      set(ref(db, `users/${user.uid}`), {
-        firstName: firstName,
-        lastName: lastName,
-        email: email,
-        uid: user.uid,
-      });
+        // Set user data in the database
+        set(ref(db, `users/${user.uid}`), {
+            firstName: firstName,
+            lastName: lastName,
+            email: email,
+            uid: user.uid,
+        });
+        window.location.href = 'login.html'
+        // Redirect to a success page
+        })
+        .catch((error) => {
+        const errorCode = error.code;
+        const errorMessage = error.message;
+        alert(errorMessage);
+        });
+    }
 
-      // Redirect to a success page
-    })
-    .catch((error) => {
-      const errorCode = error.code;
-      const errorMessage = error.message;
-      alert(errorMessage);
-    });
-}
-
-document.getElementById("signupinp").addEventListener("click", signup);
-
+    document.getElementById("signupinp").addEventListener("click", signup);

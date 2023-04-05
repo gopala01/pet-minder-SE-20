88c7b22140b16fbb979a7898cee930558d30b300
Nader Akar <91185226+NaderAkar@users.noreply.github.com>
2023-04-05 14:35:57 +0100

    Update signUpUsers.js

--- pages/authenticateUsers/signUpUsers.js
+++ pages/authenticateUsers/signUpUsers.js
@@ -1,6 +1,7 @@
 import { initializeApp } from 'https://www.gstatic.com/firebasejs/9.18.0/firebase-app.js';
 import { getAuth, createUserWithEmailAndPassword } from 'https://www.gstatic.com/firebasejs/9.18.0/firebase-auth.js';
-import { getDatabase, ref, set } from 'https://www.gstatic.com/firebasejs/9.18.0/firebase-database.js';
+import { getDatabase, ref, set, get, query, orderByChild, equalTo } from 'https://www.gstatic.com/firebasejs/9.18.0/firebase-database.js';
+
 
 const firebaseConfig = {
 apiKey: "AIzaSyBHIHD0PDT5IPyomztjZXjGY3_AdTsIfj0",
@@ -16,6 +17,38 @@ appId: "1:830582484400:web:c2a7f77ab61f1344f03a75"
 const app = initializeApp(firebaseConfig);
 const auth = getAuth(app);
 const db = getDatabase(app);
+
+async function getUserIdByEmail(targetEmail) { //Nader Added
+    const usersRef = ref(db, 'users');
+    const userQuery = query(usersRef, orderByChild('email'), equalTo(targetEmail));
+    const snapshot = await get(userQuery);
+  
+    if (snapshot.exists()) {
+      const userData = snapshot.val();
+      const userId = Object.keys(userData)[0];
+      return userId;
+    } else {
+      throw new Error('User not found');
+    }
+  }
+
+  async function requestPhoneNumber(userId) { //Nader Added
+    const userRef = ref(db, `users/${userId}`);
+    await set(userRef.child("phoneNumberVisibility"), true);
+  }
+  
+  async function getPhoneNumber(userId) {
+    const userRef = ref(db, `users/${userId}`);
+    const snapshot = await get(userRef);
+  
+    const userData = snapshot.val();
+    if (userData.phoneNumberVisibility) {
+      return userData.phoneNumber;
+    } else {
+      throw new Error("Phone number not visible");
+    }
+  }
+
 
 function signup(event) {
 event.preventDefault();
@@ -64,6 +97,7 @@ else{
         chosenOption : chosenOption,
         email: email,
         uid: user.uid,
+        phoneNumberVisibility: false, // Nader 
     });
     window.location.href = 'login.html'
     // Redirect to a success page
@@ -74,7 +108,31 @@ else{
     alert(errorMessage);
     });
 }
+
 }
 
+document.getElementById("signupinp").addEventListener("click", signup);//Nader Added
 
-document.getElementById("signupinp").addEventListener("click", signup);
+document.getElementById("requestNumberButton").addEventListener("click", async () => {
+  const targetEmail = document.getElementById("targetEmail").value;
+  try {
+    const targetUserId = await getUserIdByEmail(targetEmail);
+    await requestPhoneNumber(targetUserId);
+    alert('Phone number requested');
+  } catch (error) {
+    alert(error.message);
+  }
+});
+
+document.getElementById("getNumberButton").addEventListener("click", async () => {
+  const targetEmail = document.getElementById("targetEmail").value;
+  try {
+    const targetUserId = await getUserIdByEmail(targetEmail);
+    const phoneNumber = await getPhoneNumber(targetUserId);
+    alert(`Phone number: ${phoneNumber}`);
+  } catch (error) {
+    alert(error.message);
+  }
+});
+
+//We have to make sure the HTML file contains the necessary elements, such as an input field with the ID targetEmail, a button with the ID requestNumberButton, and another button with the ID getNumberButton.
